5b16738e9d6d2e97ae91a228fa94cb09616a4392
Jing <mqjing@gmail.com>
2024-12-07 20:36:05 +0800

    refine

--- lab/opengl.01.06-moving-8-pic/v1.2_view/src/main.cpp
+++ lab/opengl.01.06-moving-8-pic/v1.2_view/src/main.cpp
@@ -51,7 +51,7 @@ int MovePicture_1()
     }
 
     float d = 0.5f;
-    float z = -5.0f;
+    float z = -0.0f;
     MyPicture2 *myPicture = new MyPicture2((float) SCR_WIDTH, (float) SCR_HEIGHT);
     float vertices[] = { // Format: positions, texture coords
             d,  d, z,     1.0f, 1.0f,   // top right
@@ -69,42 +69,49 @@ int MovePicture_1()
     OriginXYZ *obj = new OriginXYZ();
     FPS objFPS;
 
-    float offsetX = 0.0f; // Moving triangle horizontal offset
+    // set the moving    
+    float offsetMIN = -5.0f;
+    float offsetMAX = 5.0f;
+    float offset = 0.0f; 
     bool bUP = false;
 
     // setup view & perspective
-    glm::vec3 cameraPosition = glm::vec3(1.0f, 1.0f, 3.0f); // Set the camera position at (1, 1, 3)
+    glm::vec3 cameraPosition = glm::vec3(3.0f, 3.0f, 3.0f); // glm::vec3(5.0f, 5.0f, 5.0f); // Set the camera position at (1, 1, 3)
     glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
     glm::vec3 upVector = glm::vec3(0.0f, 1.0f, 0.0f);
     glm::mat4 view = glm::lookAt(cameraPosition, cameraTarget, upVector); //  glm::mat4 view = glm::mat4(1.0f);
 
     float near = 0.01f;
-    float far = 10.0f;
+    float far = 100.0f;
+
     glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, near, far);
 
     while (!glfwWindowShouldClose(window))
     {
-         // postion
-       if (offsetX <= 0.0f && bUP == false){
+    
+        printf("offset = %f, bUP = %d\n", offset, bUP);
+        // postion
+       if (offset <= offsetMIN && bUP == false){
             bUP = true;
        }
 
-        if(offsetX > 1.0f && bUP == true){
+        if(offset > offsetMAX && bUP == true){
             bUP = false;
         }
 
         if(bUP){
-            offsetX += 0.01f;
+            offset += 0.1f;
         }else{
-            offsetX -= 0.01f;
+            offset -= 0.1f;
         }
 
+        // offset = 0;
         processInput(window); // Input processing
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
         
         
-        myPicture->display(0.0f, 0.0f, offsetX*4, view, projection);
+        myPicture->display(0.0f, 0.0f, offset, view, projection);
         obj->display(view, projection); // Render the scene
 
         glfwSwapBuffers(window); // Swap buffers
@@ -151,7 +158,7 @@ int MovePicture_9() {
 
     // create VAO, VBO from vertices
     float d = 0.5f;
-    float z = -5.0f;
+    float z = -0.0f;
     for (int i = 0; i < num; i++){
         myPictures[i] = new MyPicture2(SCR_WIDTH, SCR_HEIGHT);
         // d = d + 0.1f;
@@ -182,7 +189,7 @@ int MovePicture_9() {
     OriginXYZ *obj = new OriginXYZ();
     FPS objFPS;
 
-   float offsetX = 0.0f; // Moving triangle horizontal offset
+   float offset = 0.0f; // Moving triangle horizontal offset
    bool bUP = false;
    while (!glfwWindowShouldClose(window)) {
        // Input processing
@@ -191,48 +198,48 @@ int MovePicture_9() {
        glClear(GL_COLOR_BUFFER_BIT);
 
        // postion
-       if (offsetX <= 0.0f && bUP == false){
+       if (offset <= 0.0f && bUP == false){
             bUP = true;
        }
 
-        if(offsetX > 1.0f && bUP == true){
+        if(offset > 1.0f && bUP == true){
             bUP = false;
         }
 
         if(bUP){
-            offsetX += 0.01f;
+            offset += 0.01f;
         }else{
-            offsetX -= 0.01f;
+            offset -= 0.01f;
         }
  
        for(int i = 0; i < num; i++){
             switch(i){
             case 0:
-                myPictures[i]->display(offsetX , 0.0f , 0.0f, view, projection);
+                myPictures[i]->display(offset , 0.0f , 0.0f, view, projection);
                 break;
             case 1:
-                myPictures[i]->display(-offsetX , 0.0f , 0.0f, view, projection);
+                myPictures[i]->display(-offset , 0.0f , 0.0f, view, projection);
                 break;
             case 2:
-                 myPictures[i]->display(0.0f, offsetX, 0.0f, view, projection);
+                 myPictures[i]->display(0.0f, offset, 0.0f, view, projection);
                 break;
             case 3:
-                myPictures[i]->display(0.0f, -offsetX, 0.0f, view, projection);
+                myPictures[i]->display(0.0f, -offset, 0.0f, view, projection);
                 break;
             case 4:
-                 myPictures[i]->display(offsetX , offsetX , 0.0f, view, projection);
+                 myPictures[i]->display(offset , offset , 0.0f, view, projection);
                 break;
             case 5:
-                myPictures[i]->display(-offsetX , -offsetX , 0.0f, view, projection);
+                myPictures[i]->display(-offset , -offset , 0.0f, view, projection);
                 break;
             case 6:
-                myPictures[i]->display(-offsetX , offsetX , 0.0f, view, projection);
+                myPictures[i]->display(-offset , offset , 0.0f, view, projection);
                 break;
             case 7:
-                myPictures[i]->display(offsetX , -offsetX , 0.0f, view, projection);
+                myPictures[i]->display(offset , -offset , 0.0f, view, projection);
                 break;
             case 8:
-                myPictures[i]->display(0.9f, 0.9f, offsetX * 4.0, view, projection);
+                myPictures[i]->display(0.9f, 0.9f, offset * 4.0, view, projection);
                 break;
             default:
                 myPictures[i]->display(0.0f , 0.0f , 0.0f, view, projection);
