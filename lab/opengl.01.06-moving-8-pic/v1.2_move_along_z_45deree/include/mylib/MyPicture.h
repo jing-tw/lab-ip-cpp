2941af8e014c4256f124a2f00724907996db6a3c
Jing <mqjing@gmail.com>
2024-12-07 21:57:52 +0800

    [opengl] Add move 8 pictures along z axis with 45 degree

---
+++ lab/opengl.01.06-moving-8-pic/v1.2_move_along_z_45deree/include/mylib/MyPicture.h
@@ -0,0 +1,97 @@
+
+#include <glad/glad.h>
+
+class MyPicture2{
+    private:
+        float SCR_WIDTH = -1, SCR_HEIGHT = -1;
+        unsigned int shaderProgram;
+        GLuint VAO, VBO;
+        GLuint texture;
+
+    public:
+        MyPicture2(float scr_width, float scr_height);
+        ~MyPicture2();
+        GLuint loadTexture(const char* path, int& width, int& height);
+        bool initVAOBAO(float *vertices, unsigned int size, unsigned int attr1_element_num=3, unsigned int attr2_element_num=2);
+        bool initShader();
+        bool display(float x, float y, float z,  glm::mat4 view, glm::mat4 projection);
+};
+
+// // MyPicture: Show a png picture on screen
+// // Usage:
+// /*
+//     MyPicture *picture = new MyPicture((float) SCR_WIDTH, (float) SCR_HEIGHT);
+//     while (!glfwWindowShouldClose(window)) {
+//         processInput(window); // Input processing
+
+//         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers    
+//         picture->display(0.0f, 0.0f, 0.1f); // render the screen
+
+//         glfwSwapBuffers(window); // Swap buffers
+//         glfwPollEvents();        // Check for events
+
+//         objFPS.display(); // display fps
+//     }
+
+//     delete picture;
+// */
+// class MyPicture
+// {
+// public:
+//     float SCR_WIDTH, SCR_HEIGHT;
+//     GLuint shaderProgram;
+//     GLuint VAO;
+//     GLuint VBO;
+//     static const int VertexToalNUM = 6;                                                                                         // total vertex number
+//     static const int Vertex_Attribute1_COMPONENT_NUM = 3;                                                                       // x, y, z
+//     static const int Vertex_Attribute2_COMPONENT_NUM = 2;                                                                       // no texture location
+//     static const int Vertex_Attribute1_COMPONENT_TOTAL_NUM = Vertex_Attribute1_COMPONENT_NUM + Vertex_Attribute2_COMPONENT_NUM; // one  vector size = 3 + 0
+    
+//     float z = 0.1f;
+//     float vertices[VertexToalNUM * Vertex_Attribute1_COMPONENT_TOTAL_NUM] = {
+//     // positions                     // texture coords
+//      0.5f,  0.5f, z,  1.0f, 1.0f,   // top right
+//      0.5f, -0.5f, z,  1.0f, 0.0f,   // bottom right
+//     -0.5f, -0.5f, z,  0.0f, 0.0f,   // bottom left
+
+//      0.5f,  0.5f, z,  1.0f, 1.0f,   // top right
+//      -0.5f, -0.5f, z, 0.0f, 0.0f,   // bottom left
+//     -0.5f,  0.5f, z,  0.0f, 1.0f    // top left 
+//      };
+
+//      GLuint texture; 
+
+//     const char* vertexShaderSource = R"(
+//         #version 330 core
+//         layout(location = 0) in vec3 aPos;
+//         layout(location = 1) in vec2 aTexCoord;
+//         out vec2 TexCoord;
+//         void main() {
+//             gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+//             TexCoord = aTexCoord;
+//         }
+//     )";
+
+//    const char* fragmentShaderSource = R"(
+//        #version 330 core
+//        out vec4 FragColor;
+//        in vec2 TexCoord;
+//        uniform sampler2D texture1;
+//        void main() {
+//            FragColor = texture(texture1 , TexCoord);
+//        }
+//    )";
+// public:
+//     MyPicture(float scr_width, float scr_height);
+//     void initVAOBAO(float *vertices, unsigned int size);
+//     void display(float x, float y, float z);
+//     ~MyPicture();
+
+// private:
+//     void init();
+//     void initShader();
+//     // void initVAOBAO();
+//     GLuint loadTexture(const char* path, int& width, int& height);
+// };
+
+
---
+++ lab/opengl.01.06-moving-8-pic/v1.2_move_along_z_45deree/src/main.cpp
@@ -0,0 +1,271 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include <mylib/mylib.h>
+#include <mylib/MyFPS.h>
+#include <mylib/MyPicture.h>
+
+// Settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int MovePicture_1(); // demo move 1 picture
+int MovePicture_9(); // demo move 9 pictures
+
+int main(){
+    // return MovePicture_1();
+    return MovePicture_9();
+}
+
+
+int MovePicture_1()
+{
+    glfwInit(); // Initialize GLFW
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+#endif
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "MVP Draw Axes", nullptr, nullptr);
+
+    if (window == nullptr)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    float d = 0.5f;
+    float z = -0.0f;
+    MyPicture2 *myPicture = new MyPicture2((float) SCR_WIDTH, (float) SCR_HEIGHT);
+    float vertices[] = { // Format: positions, texture coords
+            d,  d, z,     1.0f, 1.0f,   // top right
+            d, -d, z,     1.0f, 0.0f,   // bottom right
+            -d, -d, z,     0.0f, 0.0f,  // bottom left
+
+            d,  d, z,     1.0f, 1.0f,   // top right
+            -d, -d, z,     0.0f, 0.0f,  // bottom left
+            -d,  d, z,     0.0f, 1.0f   // top left 
+            };
+    unsigned int attr1_element_num = 3;  // x, y, z
+    unsigned int attr2_element_num = 2;  // texture
+    myPicture->initVAOBAO(vertices, sizeof(vertices), attr1_element_num, attr2_element_num);
+
+    OriginXYZ *obj = new OriginXYZ();
+    MyFPS objFPS;
+
+    // set the moving    
+    float offsetMIN = -5.0f;
+    float offsetMAX = 5.0f;
+    float offset = 0.0f; 
+    bool bUP = false;
+
+    // setup view & perspective
+    glm::vec3 cameraPosition = glm::vec3(3.0f, 3.0f, 3.0f); // glm::vec3(5.0f, 5.0f, 5.0f); // Set the camera position at (1, 1, 3)
+    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 upVector = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::mat4 view = glm::lookAt(cameraPosition, cameraTarget, upVector); //  glm::mat4 view = glm::mat4(1.0f);
+
+    float near = 0.01f;
+    float far = 100.0f;
+
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, near, far);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // postion
+       if (offset <= offsetMIN && bUP == false){
+            bUP = true;
+       }
+
+        if(offset > offsetMAX && bUP == true){
+            bUP = false;
+        }
+
+        if(bUP){
+            offset += 0.1f;
+        }else{
+            offset -= 0.1f;
+        }
+
+        processInput(window); // Input processing
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color and depth buffers
+        
+        
+        myPicture->display(0.0f, 0.0f, offset, view, projection);
+        obj->display(view, projection); // Render the scene
+
+        glfwSwapBuffers(window); // Swap buffers
+        glfwPollEvents();        // Check for events
+
+        double fps = objFPS.display(false); // display fps
+        printf("fps = %f, offset = %f, bUP = %d\n", fps, offset, bUP); 
+    }
+
+    delete obj;
+    delete myPicture;
+    glfwTerminate(); // Clean up and exit the program
+    return 0;
+}
+
+
+int MovePicture_9() {
+    // init GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Display PNG", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    
+    // init GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    unsigned int num = 9;
+    MyPicture2 **myPictures = new MyPicture2* [num];
+
+    // create VAO, VBO from vertices
+    float d = 0.5f;
+    float z = -0.0f;
+    for (int i = 0; i < num; i++){
+        myPictures[i] = new MyPicture2(SCR_WIDTH, SCR_HEIGHT);
+        // d = d + 0.1f;
+        float vertices[] = { // Format: positions, texture coords
+            d,  d, z,     1.0f, 1.0f,   // top right
+            d, -d, z,     1.0f, 0.0f,   // bottom right
+            -d, -d, z,     0.0f, 0.0f,  // bottom left
+
+            d,  d, z,     1.0f, 1.0f,   // top right
+            -d, -d, z,     0.0f, 0.0f,  // bottom left
+            -d,  d, z,     0.0f, 1.0f   // top left 
+            };
+         unsigned int attr1_element_num = 3;  // x, y, z
+         unsigned int attr2_element_num = 2;  // texture
+         myPictures[i]->initVAOBAO(vertices, sizeof(vertices), attr1_element_num, attr2_element_num);
+    }
+    // setup view & perspective
+    glm::vec3 cameraPosition = glm::vec3(1.0f, 1.0f, 3.0f); // Set the camera position at (1, 1, 3)
+    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 upVector = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::mat4 view = glm::lookAt(cameraPosition, cameraTarget, upVector); //  glm::mat4 view = glm::mat4(1.0f);
+
+    float near = 0.01f;
+    float far = 10.0f;
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, near, far);
+
+
+    OriginXYZ *obj = new OriginXYZ();
+    MyFPS objFPS;
+
+   float offset = 0.0f; // Moving triangle horizontal offset
+   bool bUP = false;
+   while (!glfwWindowShouldClose(window)) {
+       // Input processing
+       processInput(window);
+
+       glClear(GL_COLOR_BUFFER_BIT);
+
+       // postion
+       if (offset <= 0.0f && bUP == false){
+            bUP = true;
+       }
+
+        if(offset > 1.0f && bUP == true){
+            bUP = false;
+        }
+
+        if(bUP){
+            offset += 0.01f;
+        }else{
+            offset -= 0.01f;
+        }
+ 
+       for(int i = 0; i < num; i++){
+            switch(i){
+            case 0:
+                myPictures[i]->display(offset , 0.0f , 0.0f, view, projection);
+                break;
+            case 1:
+                myPictures[i]->display(-offset , 0.0f , 0.0f, view, projection);
+                break;
+            case 2:
+                 myPictures[i]->display(0.0f, offset, 0.0f, view, projection);
+                break;
+            case 3:
+                myPictures[i]->display(0.0f, -offset, 0.0f, view, projection);
+                break;
+            case 4:
+                 myPictures[i]->display(offset , offset , 0.0f, view, projection);
+                break;
+            case 5:
+                myPictures[i]->display(-offset , -offset , 0.0f, view, projection);
+                break;
+            case 6:
+                myPictures[i]->display(-offset , offset , 0.0f, view, projection);
+                break;
+            case 7:
+                myPictures[i]->display(offset , -offset , 0.0f, view, projection);
+                break;
+            case 8:
+                myPictures[i]->display(0.9f, 0.9f, offset * 4.0, view, projection);
+                break;
+            default:
+                myPictures[i]->display(0.0f , 0.0f , 0.0f, view, projection);
+                break;
+            }
+       }
+
+       obj->display(view, projection);
+
+       glfwSwapBuffers(window);
+       glfwPollEvents();
+
+       // display info
+       double fps = objFPS.display(false); // fps
+       printf("fps = %f, offset = %f, bUP = %d\n", fps, offset, bUP);
+   }
+
+   // free resource
+    for(int i=0; i<num; i++){
+        delete myPictures[i];
+    }
+    delete[] myPictures; 
+
+    delete obj;
+
+   glfwTerminate();
+
+
+   return 0;
+}
